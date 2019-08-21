c6347a441b407520521d00ad0dce3398b28acf65
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-21 22:36:02 +0800

    Embed youtube video to the site

---
+++ cti-v7/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new mongoose.Schema({
+ username: String,
+ password: String,
+ avatar: String,
+ firstName: String,
+ lastName: String,
+ email: String,
+ isAdmin: {
+  type: Boolean,
+  default: false
+ }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
