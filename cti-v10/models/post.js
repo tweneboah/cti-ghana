322a618f652b34f677ef3a9594634bcd3d03ed31
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-24 07:55:59 +0800

    Profile page templated added

---
+++ cti-v10/models/post.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+let postSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   videoLink: String,
+   description: String,
+   createdAt: {
+      type: Date,
+      default: Date.now()
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ],
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'User'
+      },
+      username: String, 
+   }
+});
+
+module.exports = mongoose.model("Post", postSchema);
