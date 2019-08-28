c4563b1a79a51caf20354d2303c6584c68b3a1eb
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-08-28 19:17:36 +0800

    Deploying for testing

--- cti-v23/routes/post.js
+++ cti-v23/routes/post.js
@@ -25,7 +25,7 @@
     const upload = multer({ storage: storage, fileFilter: imageFilter})
     const cloudinary = require('cloudinary');
     cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.CLOUDINARY_API_SECRET
     });
